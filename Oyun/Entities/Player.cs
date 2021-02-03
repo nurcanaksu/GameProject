97c665196783f214c700520537aaf18746f1ba7c
nurcanaksu <nurcan.aksuf@gmail.com>
2021-02-03 19:47:02 +0300

    Create Player.cs

---
+++ Oyun/Entities/Player.cs
@@ -0,0 +1,18 @@
+﻿using Oyun.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oyun.Entities
+{
+
+    public class Player
+    {
+        public string Ad { get; set; }
+        public string SoyAd { get; set; }
+        public string TcNo { get; set; }
+        public DateTime Date{ get; set; }
+
+      
+    }
+}
